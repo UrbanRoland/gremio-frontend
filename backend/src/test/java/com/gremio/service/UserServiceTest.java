1a7bd352e33c07b15a1b3e2301d0dea80f505dee
Urban Roland <urolir@gmail.com>
2023-05-21 12:23:15 +0200

    added junit test for UserController and UserService

--- backend/src/test/java/com/gremio/service/UserServiceTest.java
+++ backend/src/test/java/com/gremio/service/UserServiceTest.java
@@ -33,30 +33,23 @@ public class UserServiceTest {
     
     @Test
     public void testGetAllUser_WithValidPageable_ReturnsUserDetailsPage() {
-            // Prepare test data
             Pageable pageable = PageRequest.of(0, 10);
             List<User> users = Arrays.asList(new User(), new User());
             Page<User> userPage = new PageImpl<>(users);
             List<UserDetailsDto> userDetails = Arrays.asList(new UserDetailsDto(), new UserDetailsDto());
             Page<UserDetailsDto> expectedPage = new PageImpl<>(userDetails);
-        
-            // Mock the userRepository to return the userPage
+            
             Mockito.when(userRepository.findAll(pageable)).thenReturn(userPage);
-        
-            // Mock the conversionService to convert each user to UserDetailsDto
+            
             Mockito.when(conversionService.convert(Mockito.any(User.class), Mockito.eq(UserDetailsDto.class)))
                     .thenReturn(new UserDetailsDto());
-        
-            // Call the getAllUser method
+            
             Page<UserDetailsDto> actualPage = userService.getAllUser(pageable);
-        
-            // Assert the expectedPage and actualPage are equal
+            
             Assertions.assertEquals(expectedPage, actualPage);
-        
-            // Verify the userRepository.findAll method was called with the correct pageable
+            
             Mockito.verify(userRepository).findAll(pageable);
-        
-            // Verify the conversionService.convert method was called for each user
+            
             Mockito.verify(conversionService, Mockito.times(users.size()))
                     .convert(Mockito.any(User.class), Mockito.eq(UserDetailsDto.class));
     
