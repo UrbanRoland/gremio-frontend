d4608dcf76ce323def8981483942cb1c9f398493
u.roli <55624995+UrbanRoland@users.noreply.github.com>
2023-12-02 18:14:57 +0100

    Delete backend directory

--- backend/src/main/java/com/gremio/facade/UserFacade.java
+++
@@ -1,7 +0,0 @@
-package com.gremio.facade;
-
-public interface UserFacade {
-    String forgotPassword(String email);
-    
-    void resetPassword(String password, String token);
-}
--- backend/src/main/java/com/gremio/service/interfaces/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.gremio.service.interfaces;
-
-import org.thymeleaf.context.Context;
-
-public interface EmailService {
-    void forgotPasswordEmail(String to, String subject, String templateName, String token, String username);
-}
